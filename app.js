4ff484579b7bc62573d12568cc742722ea7d6bfb
Soulou <leo@unbekandt.eu>
2019-03-01 10:34:33 +0100

    Wording

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/', function (req, res) {
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address
   var port = server.address().port
-  console.log('App listening at http://%s:%s', host, port)
+  console.log('App available at http://%s:%s', host, port)
 })
